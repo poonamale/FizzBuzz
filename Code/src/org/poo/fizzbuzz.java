c43d87bfb14186aaec1a9aca6ae5b11999080fac
vnguy4 <vanthoai.nguyen@aexp.com>
2021-09-16 14:10:04 +0100

    Complete rest of fizzbuzz

--- Code/src/org/poo/fizzbuzz.java
+++ Code/src/org/poo/fizzbuzz.java
@@ -2,10 +2,16 @@ package org.poo;
 
 public class fizzbuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 31 ; i++) {
-            if(i%15==0) {
+        for (int i = 1; i <= 100 ; i++) {
+            if(i % 15 == 0) {
                 System.out.println("FizzBuzz");
-            } else {System.out.println(i);}
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
